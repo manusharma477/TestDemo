187e3e13116bb8ad17eb06fc8e8680a39cef28a7
Manu Sharma <manusharma08640@gmail.com>
2026-08-03 14:03:43 +0530

    Added Commission Feature

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return "About Page";
+Route::get('/commission', function () {
+    return view('commission.index');
 });
